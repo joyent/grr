b67e477c6704609c99eaf2f6554bb70335a5dadf
Tim Foster <tim.foster@joyent.com>
2019-10-25 16:29:13 +0100

    Fix async problems. Now writing draft commit message to tempfile

--- squash-merge/squash-merge.js
+++ squash-merge/squash-merge.js
@@ -30,6 +30,10 @@ var fs = require('fs');
 var mod_vasync = require('vasync');
 var parseGitConfig = require('parse-git-config');
 var restifyClients = require('restify-clients');
+// commented out while we test. To have the temp module auto-delete the
+// tempfile for us, uncomment this
+//var temp = require('temp').track();
+var temp = require('temp');
 var VError = require('verror');
 
 // the [section] of the .gitconfig where we store properties.
@@ -70,7 +74,7 @@ var gitRepo = null;
 var prNumber = 10;
 
 // match JIRA-format ticket names, expected at the beginning of the line
-var ticketRE = new RegExp('^[A-Z]+-[0-9]+ ');
+var TICKET_RE = new RegExp('^[A-Z]+-[0-9]+ ');
 
 /*
  * Rudimentary ~ directory expansion. This doesn't work for user-relative paths
@@ -87,11 +91,16 @@ function expandTilde(path) {
 }
 
 /*
- * set gitRepo to a "gituser/gitrepo" string from the repository pointed to by
+ * Compute the "gituser/gitrepo" string from the repository pointed to by
  * process.env.GITREPO value. Eventually this should fall back to looking for
  * a git repository in $PWD as well, but we've not done that yet.
+ *
+ * returns a callback(err, standard gitHub owner/repo string )
  */
-function determineGitRepo(cb) {
+function determineGitRepo(args, cb) {
+    assert.object(args, 'arg');
+    assert.func(cb, 'cb');
+
     assert.string(process.env.GITREPO, 'Missing $GITREPO in environment');
 
     var cfgPath = expandTilde(process.env.GITREPO + '/.git/config');
@@ -120,7 +129,7 @@ function determineGitRepo(cb) {
             gitRepoName= gitRepoName.substr(0, gitRepoName.length - 4);
         }
         gitRepo = format('%s/%s', gitUser, gitRepoName);
-        cb();
+        cb(null, gitRepo);
     });
 }
 
@@ -132,8 +141,12 @@ function determineGitRepo(cb) {
  *
  * Or via $GITHUB_USER and $GITHUB_API_TOKEN_FILE environment variables.
  * With this information, initialize our restifyClient.
+ *
+ * invokes a callback(error) if any errors occur, callback() otherwise
  */
 function initializeGitClient(cb) {
+    assert.func(cb, 'cb');
+
     // Get GitHub login credentials, and initialize our restifyClient
     var gitUserConfig = parseGitConfig.sync(
         {'path': expandTilde('~/.gitconfig')});
@@ -172,111 +185,140 @@ function initializeGitClient(cb) {
 // gets miscellaneous properties from this PR, so far, the submitter and
 // PR title. Hopefully the first commit also includes the primary ticket
 // for this PR, but let's not take any chances in case it's only in the title.
-function gatherPullRequestProps(cb) {
+// requires args.gitRepo, a github standard user/repo pair.
+// returns a callback(error, submitter, title, {'ticket-id': 'ticket title', ...})
+function gatherPullRequestProps(args, cb) {
+    assert.object(args, 'args');
+    assert.string(args.gitRepo, 'args.gitRepo');
+    assert.func(cb, 'cb');
+
     gitClient.get(format('/repos/%s/pulls/%s', gitRepo, prNumber),
         function getPr(err, req, res, pr) {
+            var tickets = {};
             if (err !== null) {
                 cb(err);
                 return;
             }
             submitter = pr.user.login;
             title = pr.title;
-            if (ticketRE.test(title)) {
+            if (TICKET_RE.test(title)) {
                 tickets[(title.split(' ')[0])] = pr.title;
             }
-            cb();
+            cb(null, submitter, title, tickets);
         }
     );
 }
 
 // Gathers commit messages from the commits pushed as part of this PR
-function gatherPullRequestCommits(cb) {
-    gitClient.get(format('/repos/%s/pulls/%s/commits', gitRepo, prNumber),
+// requires args.gitRepo, a standard github user/repo pair
+//          args.tickets, any existing tickets we have
+// calls a callback(error, lastCommit, object with updated ticket info for this commit, messages)
+function gatherPullRequestCommits(args, cb) {
+    assert.object(args, 'args');
+    assert.object(args.tickets, 'args.tickets');
+    assert.string(args.gitRepo, 'args.gitRepo');
+
+    assert.func(cb, 'cb');
+    gitClient.get(format('/repos/%s/pulls/%s/commits', args.gitRepo, prNumber),
         function getPr(err, req, res, commits) {
             if (err !== null) {
                 cb(err);
                 return;
             }
+            var tickets = args.tickets;
+            var messages = [];
             commits.forEach(function processCommit(obj, index) {
                 var lines = obj.commit.message.split('\n');
                 lastCommit = obj.sha;
                 lines.forEach(function extractTickets(line) {
-                    if (ticketRE.test(line)) {
+                    if (TICKET_RE.test(line)) {
                         // record the jira ticket and full line
                         tickets[line.split(' ')[0]] = line.trim();
                     } else {
-                        // not gathering long-form commit data yet, but we'll
-                        // want to if commits start following long-form git
-                        // commit message format.
-                        log.warn('no match for line ', line);
+                        messages.push(line.trim());
                     }
                 });
             });
-            cb();
+            cb(null, lastCommit, tickets, messages);
         }
     );
 }
 
 // trawl through commits to gather reviewer info
-function gatherPullRequestReviewers(cb) {
-    gitClient.get(format('/repos/%s/pulls/%s/reviews', gitRepo, prNumber),
+// calls a callback(error, array of reviewers)
+function gatherPullRequestReviewers(args, cb) {
+    assert.object(args, 'args');
+    assert.string(args.gitRepo, 'args.gitRepo');
+    assert.func(cb, 'cb');
+
+    gitClient.get(format('/repos/%s/pulls/%s/reviews', args.gitRepo, prNumber),
         function getReviews(err, req, res, reviews) {
             if (err !== null) {
                 cb(err);
                 return;
             }
+            // we don't have a format Set object, so make do with this
+            var reviewers = {};
             reviews.forEach(function processReview(obj, index) {
                 if (obj.user.login !== submitter) {
-                    log.info('review username: ' + obj.user.login);
-                    gatherUserNameInfo(
-                        // invoke the cb() only when we've processed the
-                        // final reviewer. XXX this isn't right. We're not
-                        // always calling this on repeated runs of this program.
-                        // I still don't get async js :-(
-                        function cbOnLast(){
-                            console.log(index);
-                            if (index === reviews.length - 1) {
-                                cb();
-                            }
-                        },
-                        obj.user.login, function setName(name){
-                            reviewers[obj.user.login] = name;
-                        }
-                    );
+                    reviewers[obj.user.login] = true;
                 }
             });
+            cb(null, Object.keys(reviewers));
         }
     );
 }
 
-// get as much info about a reviewer user as we can, in order to fill out
-// "Reviewed by: [First] [Last] <email address>"
-// or fall back to:
-// "Reviewed by: [username] <email address>"
-// or finally
-// "Reviewed by: [username]"
-// Takes a callback, a user to lookup, and a function setName(full_name) which
-// gets called with the result of the user lookup.
-function gatherUserNameInfo(cb, user, setName) {
+// calls a callback (err, reviewerContacts)
+function gatherReviewerContacts(args, cb) {
+    assert.object(args, 'args');
+    assert.arrayOfString(args.reviewers, 'args.reviewers');
+    assert.func(cb, 'cb');
+
+    var reviewerContacts = {};
+    mod_vasync.forEachParallel({
+        inputs: args.reviewers,
+        // I commonly have a pattern in `vasync.forEach*`
+        // to use `nextFoo` as my callback name.
+        func: function handleOneLogin(login, nextLogin) {
+            emailContactFromUsername({user: login}, function (err, contact) {
+                if (err) {
+                    nextLogin(err);
+                } else {
+                    reviewerContacts[login] = contact;
+                    nextLogin();
+                }
+            });
+        }
+    }, function doneAllLogins(err) {
+        cb(err, reviewerContacts);
+    });
+}
+
+// Get an email contact, e.g. "John Doe <john@example.com>", from
+// a GitHub username. Fall back to just the username, or the username
+// with no email address.
+//
+// @param {Object} args.user - The github username.
+// @param {Function} cb - `function (err, contact)`
+function emailContactFromUsername(args, cb) {
+    assert.object(args, 'args');
+    assert.string(args.user, 'args.user');
+
+    var user = args.user;
+
     gitClient.get('/users/' + user,
         function getUser(err, req, res, userInfo) {
-            if (err !== null) {
+            if (err) {
                 cb(err);
                 return;
             }
-            var fullName = null;
-            var email = '';
+            var contact = userInfo.name || user;
+            if (userInfo.email) {
+                contact += ' <' + userInfo.email + '>';
+            }
 
-            if (userInfo.name !== null) {
-                fullName = userInfo.name;
-            } else {
-                fullName = user;
-            }
-            if (userInfo.email !== null) {
-                email = ' <' + userInfo.email + '>';
-            }
-            setName(fullName + email);
-            cb();
+            cb(null, contact);
         });
 }
 
@@ -286,6 +328,61 @@ function gatherUserNameInfo(cb, user, setName) {
 function gatherTicketInfo(cb) {
     ;
 }
+
+// XXX For now, this is just writing a version of commit message to a
+// tempfile and returning the path we need to use mod_async.whilst,
+// and have it prompt to load vi on the result
+function editCommitMessage(args, cb) {
+    assert.object(args, 'args')
+    assert.string(args.title, 'args.title');
+    assert.object(args.reviewerContacts, 'args.reviewerContacts');
+    assert.arrayOfString(args.messages, 'args.messages');
+    assert.func(cb, 'cb');
+
+    temp.open({suffix: '.txt'}, function(err, info) {
+        if (err) {
+            cb(err);
+            return;
+        }
+        fs.writeSync(info.fd, args.messages.join("\n"));
+        fs.writeSync(info.fd, '\n');
+        Object.keys(args.reviewerContacts).sort().forEach(function(reviewer, index) {
+                fs.writeSync(info.fd, format(
+                    'Reviewed by: %s\n', args.reviewerContacts[reviewer]));
+        });
+        fs.close(info.fd, function(err) {
+          if (err) {
+              cb(err);
+              return;
+          }
+          cb(null, info.path);
+        });
+    });
+
+}
+        // XXX just placeholder code for now
+        // // before doing this, we'll want some sort of 'is this message ok?' loop
+        // function fireUpEditor(arg, next) {
+        //     var editor = process.env.EDITOR || 'vi';
+        //     // this will eventually be editing a proper tmpfile containing
+        //     // the commit message we've built up.
+        //     var child = child_process.spawnSync(editor, ['/tmp/commitmsg.txt'], {
+        //         stdio: 'inherit'
+        //     });
+
+        //     child.on('exit', function (e, code) {
+        //         log.info('editor exited ' + code);
+        //         if (code === null) {
+        //             next();
+        //         } else {
+        //             console.log('editor didn\'t exit 0!');
+        //         }
+        //     });
+        // },
+        // // yes it is ok, now squash and merge
+        // function doMerge(arg, next) {
+        //     squashMerge(next);
+        // }
 
 // actually perform the squash+merge
 function squashMerge(cb) {
@@ -298,7 +395,7 @@ function squashMerge(cb) {
             'commit_message': 'XXX'
         },
         function putResp(err, req, res, obj) {
-            if (err !== null) {
+            if (err) {
                 cb(err);
                 return;
             }
@@ -306,82 +403,97 @@ function squashMerge(cb) {
         });
 }
 
+var context = {'cat': 'meow'};
 mod_vasync.pipeline({
-    'funcs': [
-        function getGitInfo(_, next) {
-            determineGitRepo(next);
+    arg: context,
+    funcs: [
+        function getGitInfo(arg, next) {
+            determineGitRepo(arg, function collectGitRepo(err, gitRepo) {
+                if (err) {
+                    next(err);
+                    return;
+                }
+                arg.gitRepo = gitRepo;
+                next();
+            });
         },
-        function setupClient(_, next) {
+        function setupClient(arg, next) {
             initializeGitClient(next);
         },
-        function getPrProps(_, next) {
-            gatherPullRequestProps(next);
-        },
-        function getPrCommits(_, next) {
-            gatherPullRequestCommits(next);
-        },
-        function getReviewers(_, next) {
-            gatherPullRequestReviewers(next);
-        },
-        // XXX timf: sigh, this is also not working, This time, we tried to have
-        // the previous pipeline step just populate reviewers with a set of names,
-        // intending to have this pipeline stage fill in the user details.
-        // This time we never seem to call next() at all :-/
-        // function getReviewerInfo(_, next) {
-        //     mod_vasync.forEachParallel({
-        //         'func': function gatherReviewerInfo(reviewer) {
-        //                 gatherUserNameInfo(_, reviewer, function setReviewer(name) {
-        //                     reviewers[reviewer] = name;
-        //             });
-        //         },
-        //         'inputs': Object.keys(reviewers)
-        //     }, function (err, results) {
-        //         console.log('error: %s', err.message);
-        //         console.log('results: %s', JSON.stringify(results));
-        //         next();
-        //     });
-        // },
-        function getSubmitter(_, next, submitter) {
-            if (submitter !== null) {
-                gatherUserNameInfo(next, submitter,
-                    function setSubmitter(name) {
-                        submitterName = name;
-                    });
-            } else {
-                next();
-            }
-        },
-        // before doing this, we'll want some sort of 'is this message ok?' loop
-        function fireUpEditor(_, next) {
-            var editor = process.env.EDITOR || 'vi';
-            // this will eventually be editing a proper tmpfile containing
-            // the commit message we've built up.
-            var child = child_process.spawnSync(editor, ['/tmp/commitmsg.txt'], {
-                stdio: 'inherit'
-            });
-
-            child.on('exit', function (e, code) {
-                log.info('editor exited ' + code);
-                if (code === null) {
+        function getPrProps(arg, next) {
+            gatherPullRequestProps(arg,
+                function collectProps(err, submitter, title, tickets) {
+                    if (err) {
+                        next(err);
+                        return;
+                    }
+                    arg.submitter = submitter;
+                    arg.title = title;
+                    arg.tickets = tickets;
                     next();
-                } else {
-                    console.log('editor didn\'t exit 0!');
-                }
+                });
+        },
+        function getPrCommits(arg, next) {
+            gatherPullRequestCommits(arg,
+                function collectPRCommits(err, lastCommit, tickets, msgs){
+                    if (err) {
+                        next(err);
+                        return;
+                    }
+                    arg.tickets = tickets;
+                    arg.lastCommit = lastCommit;
+                    arg.messages = msgs;
+                    next();
             });
         },
-        // yes it is ok, now squash and merge
-        function doMerge(_, next) {
-            squashMerge(next);
+        function getReviewers(arg, next) {
+            gatherPullRequestReviewers(arg,
+                function collectPRReviewers(err, reviewers){
+                    if (err) {
+                        next(err);
+                        return;
+                    }
+                    arg.reviewers = reviewers;
+                    next();
+                });
+        },
+        function getReviewerContacts(arg, next) {
+            gatherReviewerContacts(arg,
+                function collectReviewerContacts(err, reviewerContacts) {
+                    if (err) {
+                        next(err);
+                        return;
+                    }
+                    arg.reviewerContacts = reviewerContacts;
+                    next();
+                });
+        },
+        function getSubmitterContact(arg, next) {
+            emailContactFromUsername({user: arg.submitter},
+                function collectSubmitter(err, submitterContact) {
+                    if (err) {
+                        next(err);
+                        return;
+                    }
+                    arg.submitterContact = submitterContact;
+                    next();
+                });
+        },
+        function produceCommitMessage(arg, next) {
+            editCommitMessage(arg, function collectCommitMessage(err, path) {
+                if (err) {
+                    next(err);
+                    return;
+                }
+                arg.commitMessagePath = path;
+                log.info('commit message is at ' + path);
+                next();
+            });
         }
     ]
 }, function (err, results) {
         if (err) {
             assert.fail('error: %s', err.message);
         }
-        log.info(format('submitter is %s (%s)', submitterName, submitter));
-        Object.keys(reviewers).forEach(function(reviewer, index) {
-            log.info('reviewer: ' + reviewers[reviewer]);
-        });
-        log.info('title is ' + title);
-        log.info('tickets are ' + Object.keys(tickets).join(', '));
+       log.info(JSON.stringify(results));
 });
